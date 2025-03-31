1a779c8d2f70f67e2739d627b533a3d60c432dba
seungjae <dhtmdwo73@naver.com>
2025-03-31 11:43:47 +0900

    communitysave

--- src/main/java/com/example/api/entity/dto/BoardDto.java
+++ src/main/java/com/example/api/entity/dto/BoardDto.java
@@ -2,23 +2,44 @@ package com.example.api.entity.dto;
 
 import com.example.api.entity.Comment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDto {
     public static class BoardDetailResponse {
-        private Long idx;
+        private Integer idx;
         private String title; //게시판 제목
         private String content; //게시판 내용
         private String created_at;
         private String updated_at;
         private List<Comment> comments; //댓글
+        private InventoryDto.BoardResponse boardResponse; // 재료 정보
+
     }
 
     public static class BoardResponse {
-        private Long idx;
+        private Integer idx;
         private String title; //게시판 제목
         private String created_at;
         private String updated_at;
     }
+
+    public static class BoardResponseList {
+        List<BoardResponse> boardResponseList = new ArrayList<>();
+    }
+
+    public static class BoardRequest{
+        private String title;
+        private String content;
+        private InventoryDto.BoardRequest boardRequest;
+    }
+
+    public static class BoardRequestResponse {
+        private Integer idx;
+    } // 게시글 등록 요청에 대한 응답
+
+    public static class BoardDelteResponse {
+        private boolean isDeleted;
+    } // 게시글 등록 요청에 대한 응답
 
 }
--- src/main/java/com/example/api/entity/dto/CommunitySaveDto.java
+++ src/main/java/com/example/api/entity/dto/CommunitySaveDto.java
@@ -1,11 +1,24 @@
 package com.example.api.entity.dto;
 
+import java.util.List;
+
 public class CommunitySaveDto {
     public static class MyCommunitySaveResponse {
-        private Long idx;
+        private Integer idx;
         private String created_at;
     }
 
+    public static class CountResponse {
+        private Integer count;
+    }
+
+    public static class MyCommunitySaveResponseList{
+        List<MyCommunitySaveResponse> my_community_save_response;
+    }
+
+    public static class PutCommunitySaveResponse{
+        private Integer idx;
+    }
 
 
 }
